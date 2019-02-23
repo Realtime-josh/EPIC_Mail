8329563ff3fee8297faf53890b9a0c515f4c81a5
JoshTouche <franksonjoshua@gmail.com>
2019-02-23 08:49:14 +0100

    ch[responsive view sent messages]

--- UI/scripts/sentmessages.js
+++ UI/scripts/sentmessages.js
@@ -1,7 +1,4 @@
 
-// const x = document.getElementById('divinnermsg').style;
-// x.display = "none";
-
 const messageDivs = document.getElementsByClassName('innermessageDiv');
 const buttons = document.getElementsByClassName("myBtn");
 
@@ -13,26 +10,48 @@ for(let i=0;i<messageDivs.length;i++){
 
 
 
-buttons[0].addEventListener("click", function() {
-    if(messageDivs[0].style.display === "none"){
-    	messageDivs[0].style .display = "block";
+// buttons[0].addEventListener("click", function() {
+//     if(messageDivs[0].style.display === "none"){
+//     	messageDivs[0].style .display = "block";
+//     	buttons[0].innerHTML = "Collapse"
+//     }else{
+//          messageDivs[0].style .display = "none"
+//          buttons[0].innerHTML = "View Message"
+//     }
+// });
+
+// buttons[1].addEventListener("click", function() {
+//     if(messageDivs[1].style.display === "none"){
+//     	messageDivs[1].style .display = "block";
+//     	buttons[1].innerHTML = "Collapse";
+//     }else{
+//          messageDivs[1].style .display = "none"
+//          buttons[1].innerHTML = "View Message";
+//     }
+// });
+
+// buttons[2].addEventListener("click", function() {
+//      if(messageDivs[2].style.display === "none"){
+//     	messageDivs[2].style .display = "block";
+//     	buttons[2].innerHTML = "Collapse"
+//     }else{
+//          messageDivs[2].style .display = "none"
+//          buttons[2].innerHTML = "View Message"
+//     }
+// });
+
+
+
+for(let i=0; i<messageDivs.length; i++){
+	buttons[i].addEventListener("click", function() {
+    if(messageDivs[i].style.display === "none"){
+        messageDivs[i].style .display = "block";
+        buttons[i].innerHTML = "Collapse"
     }else{
-         messageDivs[0].style .display = "none"
+         messageDivs[i].style .display = "none"
+         buttons[i].innerHTML = "View Message"
     }
 });
 
-buttons[1].addEventListener("click", function() {
-    if(messageDivs[1].style.display === "none"){
-    	messageDivs[1].style .display = "block";
-    }else{
-         messageDivs[1].style .display = "none"
-    }
-});
+}
 
-buttons[2].addEventListener("click", function() {
-     if(messageDivs[2].style.display === "none"){
-    	messageDivs[2].style .display = "block";
-    }else{
-         messageDivs[2].style .display = "none"
-    }
-});
