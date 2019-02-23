ed4021ba255392d5dafea8b3f857f7169513a488
JoshTouche <franksonjoshua@gmail.com>
2019-02-23 06:53:51 +0100

    refactor

--- UI/scripts/sentmessages.js
+++ UI/scripts/sentmessages.js
@@ -1,17 +1,39 @@
 
-const x = document.getElementById('divinnermsg').style;
-x.display = "none";
+// const x = document.getElementById('divinnermsg').style;
+// x.display = "none";
 
+const x = document.getElementsByClassName('innermessageDiv');
+
+for(let i=0;i<x.length;i++){
+	x[i].style.display = "none";
+}
+
+
+
+
+
+// displayMessage = () =>{
+// 	if(x.display === "none"){
+//         document.getElementById("divinnermsg").style.display = "block";
+//         document.getElementById("seemessage1").innerHTML = "Collapse"
+//     }else if(x.display === "block"){
+//     	document.getElementById("divinnermsg").style.display = "none";
+//     	document.getElementById("seemessage1").innerHTML = "View message"
+//     }
+// }
 
 
 displayMessage = () =>{
-	if(x.display === "none"){
-        document.getElementById("divinnermsg").style.display = "block";
-        document.getElementById("seemessage1").innerHTML = "Collapse"
-    }else if(x.display === "block"){
-    	document.getElementById("divinnermsg").style.display = "none";
-    	document.getElementById("seemessage1").innerHTML = "View message"
-    }
+	for(i=0; i<x.length; i++){	    
+			if(x[i].style.display === "none"){
+		        document.getElementsByClassName(x[i]).style.display = "block";
+		        document.getElementById("seemessage1").innerHTML = "Collapse"
+		    }else if(x.style.display === "block"){
+		    	document.getElementsByClassName(x[i]).style.display = "none";
+		    	document.getElementById("seemessage1").innerHTML = "View message"
+		    }
+         
+	}
 }
 
 
