2cc1ac2137954c48ed9a8b00cb6d1263b0fb918a
JoshTouche <franksonjoshua@gmail.com>
2019-02-23 15:31:44 +0100

    [ch refactored UI]

--- UI/scripts/sentmessages.js
+++ UI/scripts/sentmessages.js
@@ -9,39 +9,6 @@ for(let i=0;i<messageDivs.length;i++){
 
 
 
-
-// buttons[0].addEventListener("click", function() {
-//     if(messageDivs[0].style.display === "none"){
-//     	messageDivs[0].style .display = "block";
-//     	buttons[0].innerHTML = "Collapse"
-//     }else{
-//          messageDivs[0].style .display = "none"
-//          buttons[0].innerHTML = "View Message"
-//     }
-// });
-
-// buttons[1].addEventListener("click", function() {
-//     if(messageDivs[1].style.display === "none"){
-//     	messageDivs[1].style .display = "block";
-//     	buttons[1].innerHTML = "Collapse";
-//     }else{
-//          messageDivs[1].style .display = "none"
-//          buttons[1].innerHTML = "View Message";
-//     }
-// });
-
-// buttons[2].addEventListener("click", function() {
-//      if(messageDivs[2].style.display === "none"){
-//     	messageDivs[2].style .display = "block";
-//     	buttons[2].innerHTML = "Collapse"
-//     }else{
-//          messageDivs[2].style .display = "none"
-//          buttons[2].innerHTML = "View Message"
-//     }
-// });
-
-
-
 for(let i=0; i<messageDivs.length; i++){
 	buttons[i].addEventListener("click", function() {
     if(messageDivs[i].style.display === "none"){
@@ -54,4 +21,60 @@ for(let i=0; i<messageDivs.length; i++){
 });
 
 }
+
+
+
+
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementsByClassName("myBtnnn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementById("close");
+
+const continueView = document.getElementById("continue");
+
+
+
+modal.style.display = "none"
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on the button, open the modal 
+for(let i=0;i<btn.length;i++){
+        btn[i].onclick = function() {
+      modal.style.display = "block";
+    }
+}
+
+
+
+continueView.onclick = function(){
+    modal.style.display = "none";
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+document.getElementById("allmessages").addEventListener("click", function(){
+  window.location.href='./dashboard.html';
+})
+
+
+
+
 
