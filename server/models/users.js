92c999f2eb710bbc7249ec6f138b05d79dea6660
JoshTouche <franksonjoshua@gmail.com>
2019-02-28 21:51:58 +0100

    FT-User-can-send-emails

--- server/models/users.js
+++ server/models/users.js
@@ -23,12 +23,58 @@ const user = {
                    ],
 
                       lastContactId : 1
+              },
+
+              {
+                    userId : 2,
+                    email : "sall@gmail.com",
+                    firstName : "Sally",
+                    lastName :  "Marcus",
+                    fullName : "Sally Marcus",
+                    password  : "yettiyea",
+
+  
+       contacts : [
+                         {
+                             id : 1,
+                             email : "candreva@gmail.com",
+                             firstName : "Candreva",
+                             lastName :  "Rodriguez",
+                             fullName : "Joshua Frankson"
+
+                         }
+                   ],
+
+                      lastContactId : 1
+              },
+
+              {
+                    userId : 3,
+                    email : "ben@gmail.com",
+                    firstName : "Ben",
+                    lastName :  "Marcus",
+                    fullName : "Ben Marcus",
+                    password  : "yettiyea",
+
+  
+       contacts : [
+                         {
+                             id : 1,
+                             email : "candreva@gmail.com",
+                             firstName : "Candreva",
+                             lastName :  "Rodriguez",
+                             fullName : "Joshua Frankson"
+
+                         }
+                   ],
+
+                      lastContactId : 1
               }
 
 
          ],
  
-              lastUserId : 1
+              lastUserId : 3
 
 };
 
@@ -36,6 +82,8 @@ const usersList = new Map();
 user.users.forEach((contactItem)=>{
 	usersList.set(contactItem.userId, contactItem);
 });
+
+
 
 
 
