38b35663fc6442919b916c16e01dad8cc4c6a5cd
JoshTouche <franksonjoshua@gmail.com>
2019-03-01 12:59:23 +0100

    user can send email

--- server/routes/message.js
+++ server/routes/message.js
@@ -43,12 +43,36 @@ messageRouter.get("/receivedmessages/:id", (req,res)=>{
           sendResponse(res,200,"No messages found for user",null);
     }
     }else{
-    	sendResponse(res,404,null,"Not Found");
+           sendResponse(res,404,null,"Not Found");
     }
-
-
-
                  
+});
+
+
+messageRouter.get("/sentemails/:id", (req,res)=>{
+     const {id} = req.params;
+     const verifyUser = user.users.filter((result)=>{
+               return result.userId === parseInt(id);
+    });
+
+     if(verifyUser.length > 0){
+            const sentMessages = Message.messages.filter((result)=>{
+                                         return result.senderId === parseInt(id);
+                                     });
+            if(sentMessages.length > 0){
+                  const getName = usersList.get(parseInt(id));
+                   res.status(200).send({
+                   status : 200,
+                   message : `All received messages for ${getName.fullName}`,
+                   sentmessages : sentMessages
+               });
+            }else{
+                    sendResponse(res,200,"No messages found for user",null);
+               }
+     }else{
+                sendResponse(res,404,null,"Not Found");
+         }
+
 });
 
 
