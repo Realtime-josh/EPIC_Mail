d4f8fb65202f7d7b90ce4f4ed1cb8997b6572805
JoshTouche <franksonjoshua@gmail.com>
2019-03-15 00:07:36 +0100

    heroku test app

--- server/app.js
+++ server/app.js
@@ -14,9 +14,9 @@ app.use('/api/v1/message', messageRouter);
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome to EPIC Mail Services' });
 });
-app.use('*', (req, res) => {
-  res.status(404).send({ error: 'Invalid Route' });
-});
+// app.use('*', (req, res) => {
+//   res.status(404).send({ error: 'Invalid Route' });
+// });
 
 app.listen(port, () => {
   console.log (`Server started on port ${port}`);
